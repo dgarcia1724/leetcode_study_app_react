012a0c0aaf6756ac284a8f1fb4db41867148abf2
Danny Garcia <dgarcia1724@berkeley.edu>
2024-10-22 18:39:03 -0700

    Main file has React Query set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
 import ToastProvider from "./components/Toast/ToastProvider.tsx";
 import { AuthProvider } from "./auth/AuthProvider.tsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ToastProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
